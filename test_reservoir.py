91fbf5307f49254f986be500c8d07904a3c4bf6c
Stan DeLaurentiis <84985760+sdelaurentiis123@users.noreply.github.com>
2026-09-17 16:45:18 -0400

    Update test_reservoir.py

--- test_reservoir.py
+++ test_reservoir.py
@@ -1,5 +1,5 @@
 import numpy as np
-from connectome_plasma.reservoir import run_reservoir
+from reservoir import run_reservoir
 
 def test_reservoir_shape():
     states = run_reservoir(np.eye(4), np.ones((5, 1)), np.ones((4, 1)))
